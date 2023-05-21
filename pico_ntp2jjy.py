a57677ce3e8fba5ff823cbe801251836b852bfd7
elehobica <elehobica@gmail.com>
2023-05-21 10:23:48 +0900

    refactor second alignment, PIO asm

--- pico_ntp2jjy.py
+++ pico_ntp2jjy.py
@@ -1,4 +1,5 @@
 # NTP to JJY
+#  for Raspberry Pi Pico W
 # ------------------------------------------------------
 # Copyright (c) 2023, Elehobica
 # ------------------------------------------------------
@@ -14,7 +15,6 @@ import time
 import utime
 import network
 import ntptime
-from micropython import const
 
 # write ssid and password as 'secrets' dict in secrets.py
 from secrets import secrets
@@ -23,8 +23,8 @@ from secrets import secrets
 TZ_JST_OFS = 9
 
 # Pin Configuration
-PIN_MOD = const(2)   # modulation (output)
-PIN_CTRL = const(3)  # control (output)
+PIN_MOD = 2   # modulation (output for PIO)
+PIN_CTRL = 3  # control (output for GPIO, input for PIO)
 
 def connectWifi():
   ssid = secrets['ssid']
@@ -47,7 +47,7 @@ def disconnectWifi():
   wlan = network.WLAN(network.STA_IF)
   wlan.deinit()
 
-# LocalTime class for NTC and RTC
+# LocalTime class for NTP and RTC
 class LocalTime:
   # utility to handle time tuple
   class TimeTuple:
@@ -71,37 +71,57 @@ class LocalTime:
         print(e)
         time.sleep(5)
         machine.reset()
-
     return self.TimeTuple(utime.localtime(utime.mktime(utime.localtime()) + offsetHour*3600))
   def __setRtc(self, t: TimeTuple) -> TimeTuple:
     machine.RTC().datetime((t.year, t.month, t.mday, t.weekday+1, t.hour, t.minute, t.second, 0))
     time.sleep(1)  # wait to be reflected
     return self.TimeTuple(time.localtime())
-  def now(self) -> TimeTuple:
-    return self.TimeTuple(time.localtime())
+  def now(self, offset: int = 0) -> TimeTuple:
+    return self.TimeTuple(time.localtime(time.time() + offset))
+  def alignSecondEdge(self):
+    t = self.now()
+    while t.second == self.now().second:
+      time.sleep_ms(1)
 
 # PIO program
 @rp2.asm_pio(set_init = rp2.PIO.OUT_LOW)
-def oscillator():
-    wrap_target()
-    wait(1, gpio, PIN_CTRL)
-    set(pins, 1) [1]
-    set(pins, 0)
-    wrap()
+def oscillatorPioAsm():
+  # generate 1/4 frequency pulse against PIO clock with enable control
+  #  wait pin from in_base
+  #  set pin from set_base
+  wrap_target()
+  wait(1, pin, 0)
+  set(pins, 1) [1]
+  set(pins, 0)
+  wrap()
 
 # JJY class
 class Jjy:
-  def __init__(self, lcTime: LocalTime, freq: int, ctrlPins: tuple(machine.Pin), modOutPin: machine.Pin):
+  def __init__(self, lcTime: LocalTime, freq: int, ctrlPins: tuple(machine.Pin), modOutPin: machine.Pin, pioAsm: Callable):
     self.lcTime = lcTime
     self.freq = freq # 40000 or 60000 (Hz)
-    self.ctrlPins = ctrlPins  # for pulse control (could be multiple)
+    self.ctrlPins = ctrlPins  # for pulse control output (could be multiple, but PIO accepts only [0] as control input)
     self.modOutPin = modOutPin  # for modulation output
-    for ctrlPin in self.ctrlPins:
-      ctrlPin.off()
-    sm = rp2.StateMachine(0, oscillator, freq=self.freq*4, set_base=self.modOutPin)
+    # dummy toggle because it spends much time (more than 0.6 sec) at first time
+    self.__control(False)
+    self.__control(True)
+    self.__control(False)
+    # start PIO
+    sm = rp2.StateMachine(0, pioAsm, freq = self.freq*4, in_base = self.ctrlPins[0], set_base = self.modOutPin)
     sm.active(True)
-  def __genTimecode(self, now: LocalTime.TimeTuple) -> None:
+  def __control(self, enable: bool) -> None:
+    for ctrlPin in self.ctrlPins:
+      if enable:
+        ctrlPin.on()
+      else:
+        ctrlPin.off()
+  def __resetVector(self):
     self.__vector = []
+  def __appendVector(self, value: int):
+    self.__vector.append(value)
+  def __getVector(self):
+    return self.__vector
+  def __genTimecode(self, t: LocalTime.TimeTuple) -> list:
     ## Timecode1 (exept for 15, 45 min) ##
     # 00: marker M
     # 01 ~ 08: Minite BCD 40, 20, 10, "0", 8, 4, 2, 1
@@ -137,34 +157,35 @@ class Jjy:
     # 80, 40, 20, 10: 10's digit
     # 8, 4, 2, 1    : 1's digit
 
-    isTimecode2 = now.minute == 15 or now.minute == 45
+    self.__resetVector()
+    isTimecode1 = not (t.minute == 15 or t.minute == 45)
     self.__addMarker()  # marker M
-    pa2 = self.__addBcd(now.minute // 10, 3)
+    pa2 = self.__addBcd(t.minute // 10, 3)
     self.__addBin(0)
-    pa2 += self.__addBcd(now.minute)
+    pa2 += self.__addBcd(t.minute)
     self.__addMarker()  # marker P1
     self.__addBin(0, 2)
-    pa1 = self.__addBcd(now.hour // 10, 2)
+    pa1 = self.__addBcd(t.hour // 10, 2)
     self.__addBin(0)
-    pa1 += self.__addBcd(now.hour)
+    pa1 += self.__addBcd(t.hour)
     self.__addMarker()  # marker P2
     self.__addBin(0, 2)
-    self.__addBcd(now.yearday // 100, 2)
+    self.__addBcd(t.yearday // 100, 2)
     self.__addBin(0)
-    self.__addBcd(now.yearday // 10)
+    self.__addBcd(t.yearday // 10)
     self.__addMarker()  # marker P3
-    self.__addBcd(now.yearday)
+    self.__addBcd(t.yearday)
     self.__addBin(0, 2)
     self.__addBin(pa1) # PA1
     self.__addBin(pa2) # PA2
-    if not isTimecode2:
+    if isTimecode1:
       self.__addBin(0) # SU1
       self.__addMarker()  # marker P4
       self.__addBin(0) # SU2
-      self.__addBcd(now.year // 10)
-      self.__addBcd(now.year)
+      self.__addBcd(t.year // 10)
+      self.__addBcd(t.year)
       self.__addMarker()  # marker P5
-      self.__addBcd((now.weekday + 1) % 7, 3)
+      self.__addBcd((t.weekday + 1) % 7, 3)
       self.__addBin(0) # LS1
       self.__addBin(0) # LS2
       self.__addBin(0, 4)
@@ -176,25 +197,24 @@ class Jjy:
       self.__addBin(0, 6) # ST1 ~ ST6
       self.__addBin(0, 3)
     self.__addMarker()  # marker P0
+    return self.__getVector()
   def __addMarker(self) -> None:
-    self.__vector.append(2)
+    self.__appendVector(2)
   def __addBcd(self, value: int, numDigits: int = 4) -> int:
     parity = 0
     value = value % 10
-    bitPos = numDigits - 1
-    while bitPos >= 0:
+    for bitPos in range(numDigits-1, -1, -1):
       bit = (value >> bitPos) & 0b1
-      self.__vector.append(bit)
+      self.__appendVector(bit)
       parity += bit
-      bitPos -= 1
     return parity
   def __addBin(self, value: int, count: int = 1) -> None:
     for i in range(count):
-      self.__vector.append(value & 0b1)
-  def __sendTimecode(self, fromSec: int) -> None:
-    for value in self.__vector[fromSec:]:
-      for ctrlPin in self.ctrlPins:
-        ctrlPin.on()
+      self.__appendVector(value & 0b1)
+  def __sendTimecode(self, vector: list) -> None:
+    for value in vector:
+      self.lcTime.alignSecondEdge()
+      self.__control(True)
       if value == 0:  # bit 0
         pulseWidth = 0.8
       elif value == 1:  # bit 1
@@ -202,28 +222,19 @@ class Jjy:
       else:  # marker
         pulseWidth = 0.2
       time.sleep(pulseWidth)
-      for ctrlPin in self.ctrlPins:
-        ctrlPin.off()
-      self.millisPrev = time.ticks_add(self.millisPrev, 1000)
-      while time.ticks_diff(time.ticks_ms(), self.millisPrev) < 0:
-        time.sleep_ms(1)
+      self.__control(False)
   def run(self):
-    print(f'start emission at {self.freq} Hz')
-    # millisecond edge alignment
-    now = self.lcTime.now()
-    while now.second == self.lcTime.now().second:
-      time.sleep_ms(1)
-    self.millisPrev = time.ticks_ms()
-    # emit timecode immediately
+    print(f'start JJY emission at {self.freq} Hz')
+    self.lcTime.alignSecondEdge()
+    time.sleep(0.2)  # to make same condition as marker P0
     while True:
-      now = self.lcTime.now()
-      print(now)
-      self.__genTimecode(now)
-      secOffset = now.second
-      self.__sendTimecode(secOffset)
+      t = self.lcTime.now(1)  # time for next second
+      vector = self.__genTimecode(t)
+      print(f'Timecode: {t}')
+      self.__sendTimecode(vector[t.second:])  # apply offset (should be only for the first time)
 
 def main() -> bool:
-  machine.freq(96000000) # multiplier of 40000*4 and 60000*4
+  machine.freq(96000000)  # multiplier of 40000*4 and 60000*4 to avoid jitter
   led = machine.Pin("LED", machine.Pin.OUT)
   led.off()
   # connect WiFi
@@ -233,16 +244,17 @@ def main() -> bool:
   for i in range(2 * 3):
     time.sleep(0.1)
     led.toggle()
-  # NTC/RTC setting
+  # NTP/RTC setting
   lcTime = LocalTime(TZ_JST_OFS)
   # disconnect WiFi
   disconnectWifi()
   # JJY
   jjy = Jjy(
-    lcTime=lcTime,
-    freq=40000, # 40000 or 60000
-    ctrlPins=(led, machine.Pin(PIN_CTRL, machine.Pin.OUT)),
-    modOutPin=machine.Pin(PIN_MOD, machine.Pin.OUT),
+    lcTime = lcTime,
+    freq = 40000,  # 40000 or 60000
+    ctrlPins = (machine.Pin(PIN_CTRL, machine.Pin.OUT), led),
+    modOutPin = machine.Pin(PIN_MOD, machine.Pin.OUT),
+    pioAsm = oscillatorPioAsm,
   )
   jjy.run()  # infinite loop
   return True
